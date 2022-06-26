a951dcfd121c94a3eea8655aabd836d088db45a2
acanna2013 <alchen@purdue.edu>
2022-06-26 15:54:34 -0400

    add emotional gifs

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -17,30 +17,27 @@ module.exports = async function (context, req) {
   const result = await analyzeImage(parts[0].data);
   context.res = {
     body: {
-      result,
-    },
+      result
+    }
   };
   console.log(result);
   context.done();
 }
 
 async function analyzeImage(img) {
-  // const subscriptionKey = process.env.SUBSCRIPTIONKEY;
-  const subscriptionKey = "822bc40507aa4fc6920b60ea63f87e2f";
-  // const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
-  const uriBase =
-    "https://annasfaceapi.cognitiveservices.azure.com/" + "/face/v1.0/detect";
+   const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+   const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
   let params = new URLSearchParams({
     returnFaceId: "true",
-    returnFaceAttributes: "emotion", //FILL IN THIS LINE
-  });
+    returnFaceAttributes: 'emotion', //FILL IN THIS LINE
+  })
   let resp = await fetch(uriBase + "?" + params.toString(), {
     method: "POST", //WHAT TYPE OF REQUEST?
     body: "img", //WHAT ARE WE SENDING TO THE API?
     headers: {
-      "Content-Type": 'application/octet-stream', //do this in the next section
-      "0cp-Apim-Subscription-Key": subscriptionKey,
-    },
+      'Content-Type': 'application/octet-stream', //do this in the next section
+      "0cp-Apim-Subscription-Key": subscriptionKey
+    }
   });
   let data = await resp.json();
 
