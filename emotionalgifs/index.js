df081ccdbf278343ab3b9a9e051a47dd0ceb0cfb
acanna2013 <alchen@purdue.edu>
2022-06-26 16:06:53 -0400

    emotioanl gifs code

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,44 +1,101 @@
-const multipart = require("parse-multipart");
-const fetch = require("node-fetch");
+//const multipart = require("parse-multipart");
+//const fetch = require("node-fetch");
+//module.exports = async function (context, req) {
+//  context.log("JavaScript HTTP trigger function processed a request.");
+//
+//  // here's your boundary:
+//  const boundary = multipart.getBoundary(req.headers["content-type"]);
+//
+//  // TODO: assign the body variable the correct value
+//  const body = req.body;
+//
+//  // parse the body
+//  const parts = multipart.Parse(body, boundary);
+//  // FILL IN THE BLANK
+//  //module.exports function
+//  //analyze the image
+//  const result = await analyzeImage(parts[0].data);
+//  context.res = {
+//    body: {
+//      result
+//    }
+//  };
+//  console.log(result);
+//  context.done();
+//}
+//
+//async function analyzeImage(img) {
+//   const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+//   const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+//  let params = new URLSearchParams({
+//    returnFaceId: "true",
+//    returnFaceAttributes: 'emotion' //FILL IN THIS LINE
+//  })
+//  let resp = await fetch(uriBase + "?" + params.toString(), {
+//    method: "POST", //WHAT TYPE OF REQUEST?
+//    body: "img", //WHAT ARE WE SENDING TO THE API?
+//    headers: {
+//     '<HEADER NAME>': '<HEADER VALUE>'
+//    }
+//  });
+//  let data = await resp.json();
+//
+//  return data;
+//}
+//
+
+const multipart = require('parse-multipart');
+const fetch = require('node-fetch');
+
 module.exports = async function (context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-  // here's your boundary:
-  const boundary = multipart.getBoundary(req.headers["content-type"]);
+    const boundary = multipart.getBoundary(req.headers['content-type']);
+    const body = req.body;
+    const parts = multipart.Parse(body, boundary);
 
-  // TODO: assign the body variable the correct value
-  const body = req.body;
+    let result = await analyzeImage(parts[0].data);
 
-  // parse the body
-  const parts = multipart.Parse(body, boundary);
-  // FILL IN THE BLANK
-  //module.exports function
-  //analyze the image
-  const result = await analyzeImage(parts[0].data);
-  context.res = {
-    body: {
-      result
-    }
-  };
-  console.log(result);
-  context.done();
+    let emotions = result[0].faceAttributes.emotion;
+    let objects = Object.values(emotions);
+    const main_emotion = Object.keys(emotions).find(key => emotions[key] === Math.max(...objects));
+    const API_KEY = process.env.GIFKEY;
+    const resp = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=" + API_KEY + 
+    "&limit=1&s="
+     + main_emotion);
+    const jsonData = await resp.json();
+
+    context.log(jsonData);
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: 
+        jsonData.data.url
+    };
+    console.log(result)
+    context.done();
 }
 
 async function analyzeImage(img) {
-   const subscriptionKey = process.env.SUBSCRIPTIONKEY;
-   const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
-  let params = new URLSearchParams({
-    returnFaceId: "true",
-    returnFaceAttributes: 'emotion' //FILL IN THIS LINE
-  })
-  let resp = await fetch(uriBase + "?" + params.toString(), {
-    method: "POST", //WHAT TYPE OF REQUEST?
-    body: "img", //WHAT ARE WE SENDING TO THE API?
-    headers: {
-     '<HEADER NAME>': '<HEADER VALUE>'
-    }
-  });
-  let data = await resp.json();
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+    
+    
 
-  return data;
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'     //FILL IN THIS LINE
+    })
+
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',  //WHAT TYPE OF REQUEST?
+        body: img,  //WHAT ARE WE SENDING TO THE API?
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            "Ocp-Apim-Subscription-Key": subscriptionKey
+        }
+    })
+    let data = await resp.json();
+
+    return data;
 }
